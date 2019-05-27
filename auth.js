f113ce84b5beac8d147c95fd81564881e1d9888c
MarJan31 <42395525+MarJan31@users.noreply.github.com>
2019-05-27 03:18:55 -0500

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,18 +1,13 @@
-
-
-// signup
 const signupForm = document.querySelector('#form-signin');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    // get user info
     const email = signupForm['email'].value;
     const password = signupForm['password'].value;
 
-    // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
         console.log(cred.user);
-        // close the signup modal & reset form
+
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
