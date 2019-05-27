2d7aac62e83770519bb79e3b67234b4442dc86fd
MarJan31 <42395525+MarJan31@users.noreply.github.com>
2019-05-27 01:59:48 -0500

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,16 @@
+<!--SignUp-->
+const signupForm = document.querySelector('#form-signin');
+signupForm.addAll('submit', (e) => {
+    e.preventDefault();
+
+    const email = signupForm['email'].value;
+    const password = signupForm['password'].value;
+
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+});
